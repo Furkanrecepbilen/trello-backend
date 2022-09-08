e975b09ad7230c3450179b199845786644330be9
Furkanrecepbilen <110205212+Furkanrecepbilen@users.noreply.github.com>
2022-09-09 01:03:13 +0300

    Add files via upload

---
+++ src/api/common/errorCodes.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ERR_00001: { key: "ERR_00001", value: "token_expired" },
+  ERR_00002: { key: "ERR_00002", value: "controller_error" },
+  ERR_00003: { key: "ERR_00003", value: "check_input_fields" },
+};
---
+++ src/api/languages/en.js
@@ -0,0 +1,5 @@
+module.exports = {
+  token_expired: "Your session has expired",
+  controller_error: "Something went wrong",
+  check_input_fields: "Check your inputs",
+};
